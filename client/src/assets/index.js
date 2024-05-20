44f9edfe6eab805b910dcb2cdfcc656362bc740a
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2024-05-20 16:15:08 +0530

    Initial commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,18 @@
+import threeDotLightLogo from "./lightmode/icons8-ellipsis-60.png";
+import filterLightLogo from "./lightmode/icons8-filter-50.png";
+import searchLightLogo from "./lightmode/icons8-search-50.png";
+import sortLightLogo from "./lightmode/icons8-sort-50.png";
+import sunLogo from "./lightmode/icons8-sun-96.png";
+
+import plusLogo from "./icons8-plus-48.png";
+import userLogo from "./icons8-user-96.png";
+
+export {
+  threeDotLightLogo,
+  filterLightLogo,
+  searchLightLogo,
+  sortLightLogo,
+  sunLogo,
+  userLogo,
+  plusLogo,
+};
