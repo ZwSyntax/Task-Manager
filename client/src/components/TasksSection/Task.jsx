895738b84db27ac2c084bea46489e53509331e27
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2024-05-21 13:03:41 +0530

    navbar , task table responsiveness primary done

--- client/src/components/TasksSection/Task.jsx
+++ client/src/components/TasksSection/Task.jsx
@@ -50,137 +50,140 @@ const Task = ({ editTaskHandler }) => {
   return (
     <div className={styles["task-container"]}>
       <table>
-        <tr>
-          <th>Task</th>
-          <th>Date</th>
-          <th>Status</th>
-          <th>Priority</th>
-          <th>
-            <img
-              src={threeDotLightLogo}
-              className={styles["three-dot-header"]}
-              alt={"three dot"}
-            />
-          </th>
-        </tr>
-
-        <tr>
-          <td>Design Wireframe</td>
-          <td>Sep 04, 2018</td>
-          <td>
-            <div className={styles["progress-section"]}>
-              <div className={styles["dot"]}></div>
-              <p>In Progress</p>
-            </div>
-          </td>
-          <td>
-            <div className={styles["priority-flag"]}>
-              <img src={redFlag} alt={"red flag"} />
-            </div>
-          </td>
-          <td>
-            <div className={styles["three-dot-option-section"]}>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Date</th>
+            <th>Status</th>
+            <th>Priority</th>
+            <th>
               <img
-                className={styles["three-dot-option-img"]}
                 src={threeDotLightLogo}
-                alt={"threeDotLight"}
+                className={styles["three-dot-header"]}
+                alt={"three dot"}
               />
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>Design Wireframe</td>
-          <td>Sep 04, 2018</td>
-          <td>
-            <div className={styles["progress-section"]}>
-              <div className={styles["dot"]}></div>
-              <p>In Progress</p>
-            </div>
-          </td>
-          <td>
-            <div className={styles["priority-flag"]}>
-              <img src={redFlag} alt={"red flag"} />
-            </div>
-          </td>
-          <td>
-            <div
-              className={styles["three-dot-option-section"]}
-              onClick={(e) => optionShowHandler(e, true)}
-            >
-              <img
-                className={styles["three-dot-option-img"]}
-                src={threeDotLightLogo}
-                alt={"threeDotLight"}
-              />
-              {isOptionShown && (
-                <div
-                  className={styles["three-dot-option"]}
-                  onMouseLeave={(e) => optionShowHandler(e, false)}
-                >
-                  <p
-                    onClick={(e) => {
-                      optionShowHandler(e, false);
-                      editTaskHandler();
-                    }}
-                  >
-                    Edit
-                  </p>
-                  <p
-                    onClick={(e) => {
-                      optionShowHandler(e, false);
-                      onDeleteClicked(e);
-                    }}
-                  >
-                    Delete
-                  </p>
-                  <p
-                    onClick={(e) => {
-                      optionShowHandler(e, false), onCompleteClicked(e);
-                    }}
-                  >
-                    Marked Complete
-                  </p>
-                </div>
-              )}
-              {isDeleteConfirmShow && (
-                <TaskOptionConfirmCard
-                  closeHander={confirmOptionCloseHandler}
-                  optionHandeler={deleteOnConfirmHandler}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Design Wireframe This is for my demo purpose where i want</td>
+            <td>Sep 04, 2018</td>
+            <td>
+              <div className={styles["progress-section"]}>
+                <div className={styles["dot"]}></div>
+                <p>In Progress</p>
+              </div>
+            </td>
+            <td>
+              <div className={styles["priority-flag"]}>
+                <img src={redFlag} alt={"red flag"} />
+              </div>
+            </td>
+            <td>
+              <div className={styles["three-dot-option-section"]}>
+                <img
+                  className={styles["three-dot-option-img"]}
+                  src={threeDotLightLogo}
+                  alt={"threeDotLight"}
                 />
-              )}
-              {isCompleteConfirmShow && (
-                <TaskOptionConfirmCard
-                  closeHander={confirmOptionCloseHandler}
-                  optionHandeler={completeOnConfirmHandler}
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Design Wireframe</td>
+            <td>Sep 04, 2018</td>
+            <td>
+              <div className={styles["progress-section"]}>
+                <div className={styles["dot"]}></div>
+                <p>In Progress</p>
+              </div>
+            </td>
+            <td>
+              <div className={styles["priority-flag"]}>
+                <img src={redFlag} alt={"red flag"} />
+              </div>
+            </td>
+            <td>
+              <div
+                className={styles["three-dot-option-section"]}
+                onClick={(e) => optionShowHandler(e, true)}
+              >
+                <img
+                  className={styles["three-dot-option-img"]}
+                  src={threeDotLightLogo}
+                  alt={"threeDotLight"}
                 />
-              )}
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>Design Wireframe</td>
-          <td>Sep 04, 2018</td>
-          <td>
-            <div className={styles["progress-section"]}>
-              <div className={styles["dot"]}></div>
-              <p>In Progress</p>
-            </div>
-          </td>
-          <td>
-            <div className={styles["priority-flag"]}>
-              <img src={redFlag} alt={"red flag"} />
-            </div>
-          </td>
-          <td>
-            <div className={styles["three-dot-option-section"]}>
-              <img
-                className={styles["three-dot-option-img"]}
-                src={threeDotLightLogo}
-                alt={"threeDotLight"}
-              />
-            </div>
-          </td>
-        </tr>
+                {isOptionShown && (
+                  <div
+                    className={styles["three-dot-option"]}
+                    onMouseLeave={(e) => optionShowHandler(e, false)}
+                  >
+                    <p
+                      onClick={(e) => {
+                        optionShowHandler(e, false);
+                        editTaskHandler();
+                      }}
+                    >
+                      Edit
+                    </p>
+                    <p
+                      onClick={(e) => {
+                        optionShowHandler(e, false);
+                        onDeleteClicked(e);
+                      }}
+                    >
+                      Delete
+                    </p>
+                    <p
+                      onClick={(e) => {
+                        optionShowHandler(e, false), onCompleteClicked(e);
+                      }}
+                    >
+                      Marked Complete
+                    </p>
+                  </div>
+                )}
+                {isDeleteConfirmShow && (
+                  <TaskOptionConfirmCard
+                    closeHander={confirmOptionCloseHandler}
+                    optionHandeler={deleteOnConfirmHandler}
+                  />
+                )}
+                {isCompleteConfirmShow && (
+                  <TaskOptionConfirmCard
+                    closeHander={confirmOptionCloseHandler}
+                    optionHandeler={completeOnConfirmHandler}
+                  />
+                )}
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Design Wireframe</td>
+            <td>Sep 04, 2018</td>
+            <td>
+              <div className={styles["progress-section"]}>
+                <div className={styles["dot"]}></div>
+                <p>In Progress</p>
+              </div>
+            </td>
+            <td>
+              <div className={styles["priority-flag"]}>
+                <img src={redFlag} alt={"red flag"} />
+              </div>
+            </td>
+            <td>
+              <div className={styles["three-dot-option-section"]}>
+                <img
+                  className={styles["three-dot-option-img"]}
+                  src={threeDotLightLogo}
+                  alt={"threeDotLight"}
+                />
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
